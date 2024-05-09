016d706a039af913d527adf51aa16b1e2db00d48
Jordan Welsman <jordan.welsman@hotmail.co.uk>
2024-05-09 21:54:16 +0100

    build: Incremented version number.

--- spectare/__init__.py
+++ spectare/__init__.py
@@ -2,15 +2,15 @@
 # Spectare
 
 The spectare module contains a collection of functions and classes
-for visualising neural networks created using the _PyTorch_ and
-_TensorFlow_ libraries. The module is designed to be used in
-conjuntion with the _Jupyter_ notebook environment, and is
+for visualising neural networks created using the PyTorch and
+TensorFlow libraries. The module is designed to be used in
+conjuntion with the Jupyter notebook environment, and is
 intended to provide a simple and intuitive way to visualise the
 structure of neural networks.
 """
 
 # Dunder attributes
-__version__ = "v0.1.0" # update setup.py
+__version__ = "v2.0.0"
 __author__ = "Jordan Welsman"
 
 # Module Imports
